de9b857444b3bb1ff8b847dc5058036feded0714
Alexey Kulish <alexkulish1993@gmail.com>
2022-02-20 11:30:52 +0300

    Initial Commit

---
+++ ChangeColorsSwiftUI/ChangeColorsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeColorsSwiftUIApp.swift
+//  ChangeColorsSwiftUI
+//
+//  Created by Alex Kulish on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChangeColorsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
